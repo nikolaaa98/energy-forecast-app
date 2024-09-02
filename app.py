c617c6231a8c1504a038a304cafff2160f4fcc3d
Mac <mac@Macs-MacBook-Pro.local>
2024-09-02 20:37:15 +0200

    Initial setting

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///energy.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from routes import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
